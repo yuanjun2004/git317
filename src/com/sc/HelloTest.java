efa57055b13e9a73c4c4232af1ecb1772886796f
yuanjun <370040986@qq.com>
2023-03-17 09:30:52 +0800

    add message

--- src/com/sc/HelloTest.java
+++ src/com/sc/HelloTest.java
@@ -2,6 +2,9 @@ package com.sc;
 
 public class HelloTest {
     public static void main(String[] args) {
+
         System.out.println("hello 317!");
+
+        System.out.println("9:29");
     }
 }
